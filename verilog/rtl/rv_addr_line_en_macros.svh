d203e8ac336f7f4f9c46f26d4871236fa8b79e02
Matthew Ballance <matt.ballance@gmail.com>
2021-01-28 09:24:36 -0800

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- verilog/rtl/rv_addr_line_en_macros.svh
+++ verilog/rtl/rv_addr_line_en_macros.svh
@@ -26,6 +26,14 @@
 	wire							PREFIX``valid; \
 	wire							PREFIX``ready
 	
+`define RV_ADDR_LINE_EN_WIRES_ARR(PREFIX,ADR_WIDTH,DAT_WIDTH,SIZE) \
+	wire[SIZE*ADR_WIDTH-1:0]				PREFIX``adr; \
+	wire[SIZE*DAT_WIDTH-1:0]				PREFIX``dat_w; \
+	wire[SIZE*DAT_WIDTH-1:0]				PREFIX``dat_r; \
+	wire[SIZE-1:0]							PREFIX``we; \
+	wire[SIZE-1:0]							PREFIX``valid; \
+	wire[SIZE-1:0]							PREFIX``ready
+	
 `define RV_ADDR_LINE_EN_CONNECT(P_PREFIX, W_PREFIX) \
 	.P_PREFIX``adr( W_PREFIX``adr ), \
 	.P_PREFIX``dat_w( W_PREFIX``dat_w ), \
@@ -33,5 +41,13 @@
 	.P_PREFIX``we( W_PREFIX``we ), \
 	.P_PREFIX``valid( W_PREFIX``valid ), \
 	.P_PREFIX``ready( W_PREFIX``ready )
+	
+`define RV_ADDR_LINE_EN_CONNECT_ARR(P_PREFIX, W_PREFIX, INDEX, ADR_WIDTH, DAT_WIDTH) \
+	.P_PREFIX``adr( W_PREFIX``adr[(INDEX)*(ADR_WIDTH)+:(ADR_WIDTH)] ), \
+	.P_PREFIX``dat_w( W_PREFIX``dat_w[(INDEX)*(DAT_WIDTH)+:(DAT_WIDTH)] ), \
+	.P_PREFIX``dat_r( W_PREFIX``dat_r[(INDEX)*(DAT_WIDTH)+:(DAT_WIDTH)] ), \
+	.P_PREFIX``we( W_PREFIX``we[INDEX] ), \
+	.P_PREFIX``valid( W_PREFIX``valid[INDEX] ), \
+	.P_PREFIX``ready( W_PREFIX``ready[INDEX] )
 	
 `endif /* INCLUDED_RV_ADDR_LINE_EN_MACROS_SVH */
